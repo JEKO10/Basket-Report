830339a7ee7668a32fcada9c86225240725cf902
Aleksa Bubanja <aleksa3b@gmail.com>
2024-01-29 23:33:30 +0100

    inserting user information into the database through the React form

--- server/server.js
+++ server/server.js
@@ -1,20 +1,32 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
-
 const db = mysql.createPool({
   host: process.env.dbHost,
   user: process.env.dbUser,
   password: process.env.dbPassword,
   database: process.env.dbDatabase,
 });
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("aleska");
+app.post("/api/insert", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const sqlInsert =
+    "INSERT INTO users (username, email, password) VALUES (?, ?, ?);";
+
+  db.query(sqlInsert, [username, email, password], (err, result) => {
+    console.log(err);
+  });
 });
 
 app.listen(PORT, () => {
