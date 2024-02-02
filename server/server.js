aa4efcbe98fa8867bf201d7e30069816e38a4d1f
Aleksa <aleksa3b@gmail.com>
2024-02-02 12:22:44 +0100

    mysql connection fix

--- server/server.js
+++ server/server.js
@@ -7,14 +7,23 @@ const mysql = require("mysql");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const db = mysql.createPool({
-  host: process.env.dbHost,
   user: process.env.dbUser,
+  host: process.env.dbHost,
   password: process.env.dbPassword,
   database: process.env.dbDatabase,
 });
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+  } else {
+    console.log("Connected to MySQL!");
+    connection.release();
+  }
+});
 
 app.post("/register", (req, res) => {
   let { username, email, password } = req.body;
@@ -26,7 +35,12 @@ app.post("/register", (req, res) => {
     "INSERT INTO users (username, email, password) VALUES (?, ?, ?);",
     [username, email, password],
     (err, result) => {
-      res.send({ message: err });
+      if (err) {
+        console.error("Error executing query:", err);
+        res.status(500).send({ message: "Internal Server Error" });
+      } else {
+        res.send({ message: "User registered successfully" });
+      }
     }
   );
 });
