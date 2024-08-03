33c91c9a4ffe46e8d4b9f0a016a574be194e01f5
Aleksa <aleksa3b@gmail.com>
2024-08-03 14:38:22 +0200

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import React from "react";
+
 export default function Home() {
-  return <main>a</main>;
+  return (
+    <div>
+      <aside className="bg-background my-4 px-5 py-5 w-56 rounded-lg">
+        <ul className="text-primary text-2xl">
+          <li>Početna</li>
+          <li>O nama</li>
+          <li>Korisnici</li>
+          <li>Napravi turnir</li>
+        </ul>
+      </aside>
+    </div>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { MdOutlineSportsBasketball } from "react-icons/md";
+import { MdOutlineSportsVolleyball } from "react-icons/md";
 
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  const [isBasketball, setIsBasketball] = useState(true);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setIsBasketball(!isBasketball);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [isBasketball]);
+
+  return (
+    <nav className="flex items-end justify-start bg-primary px-5 py-5 h-52 rounded-lg">
+      <div className="flex items-center justify-start">
+        {isBasketball ? (
+          <MdOutlineSportsBasketball className="text-4xl text-text mr-1 -rotate-45" />
+        ) : (
+          <MdOutlineSportsVolleyball className="text-4xl text-text mr-1 -rotate-45" />
+        )}
+        <p className="text-4xl text-text">Turniri</p>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
