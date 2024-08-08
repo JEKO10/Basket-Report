c0aa995977c25c11b29c16995894b24817955e70
Aleksa <aleksa3b@gmail.com>
2024-08-08 18:43:49 +0200

    control buttons

--- app/(auth)/new/page.tsx
+++ app/(auth)/new/page.tsx
@@ -3,11 +3,21 @@
 import React from "react";
 
 import Navbar from "@/components/Navbar";
+import TournamentForm from "@/components/TournamentForm";
 
 const NewPage = () => {
   return (
     <div>
       <Navbar />
+      <TournamentForm />
+      <article className="mb-8">
+        <button className="bg-primary text-text text-xl italic font-medium px-4 py-2 mr-10 rounded-lg hover:bg-background">
+          Prethodno
+        </button>
+        <button className="bg-accent text-xl italic font-medium tracking-wider px-4 py-2 mr-10 rounded-lg hover:bg-primary">
+          Sledeće
+        </button>
+      </article>
     </div>
   );
 };
