a599cb8467f8bc23a66b5f0db323bcf43a7d483b
Aleksa <aleksa3b@gmail.com>
2024-08-21 22:27:20 +0200

    add links in auth pages

--- components/authForm/Form.tsx
+++ components/authForm/Form.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { LuDoorOpen } from "react-icons/lu";
 
@@ -12,11 +13,19 @@ const FormAuth = ({ label, children }: FormAuthProps) => {
       <header className="flex items-end justify-start font-lusitana bg-primary px-6 py-4 h-36 rounded-lg">
         <p className="text-4xl text-white">{label}</p>
       </header>
-      <form className="flex items-start justify-center flex-col bg-accent mt-2 px-8 pt-7 pb-7 rounded-lg">
+      <form className="flex items-start justify-center flex-col bg-accent mt-2 px-8 pt-7 pb-5 rounded-lg">
         {children}
+        <Link
+          href={`${label === "Prijavi se" ? "/register" : "/login"}`}
+          className="text-md italic font-medium text-[#6eabda] underline -mt-1"
+        >
+          {label === "Prijavi se"
+            ? "Nemate nalog? Napravite jedan odmah..."
+            : "Već imate nalog?"}
+        </Link>
         <button
           type="submit"
-          className="flex items-center justify-between bg-primary mt-2 w-full text-text text-lg py-2 px-3 rounded-md transition hover:bg-primary/65"
+          className="flex items-center justify-between bg-primary mt-8 w-full text-text text-lg py-2 px-3 rounded-md transition hover:bg-primary/65"
         >
           <span className="text-sm font-medium">Prijavi se</span>
           <LuDoorOpen />
