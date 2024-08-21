44abb090ebcaa71f05bdad796b40ee006ef4a841
Aleksa <aleksa3b@gmail.com>
2024-08-21 23:00:06 +0200

    login and register zod schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,12 @@
 import * as z from "zod";
 
+export interface TournamentProps {
+  formData: z.infer<typeof TournamentSchema>;
+  setFormData: React.Dispatch<
+    React.SetStateAction<z.infer<typeof TournamentSchema>>
+  >;
+}
+
 export const TournamentSchema = z.object({
   tournamentType: z.number({
     message: "Format je obavezan!",
@@ -10,7 +17,7 @@ export const TournamentSchema = z.object({
       message: "Ime je obavezno!",
     })
     .min(3, {
-      message: "Ime mora da sadrži najmanje 3 slova!",
+      message: "Minimum 3 slova!",
     }),
   participants: z.number().min(2),
   teams: z.array(z.string()),
@@ -18,9 +25,23 @@ export const TournamentSchema = z.object({
   randomize: z.boolean(),
 });
 
-export interface TournamentProps {
-  formData: z.infer<typeof TournamentSchema>;
-  setFormData: React.Dispatch<
-    React.SetStateAction<z.infer<typeof TournamentSchema>>
-  >;
-}
+export const LoginSchema = z.object({
+  email: z.string().email({
+    message: "Email je obavezan!",
+  }),
+  password: z.string().min(6, {
+    message: "Minimum 6 slova!",
+  }),
+});
+
+export const RegisterSchema = z.object({
+  username: z.string().min(1, {
+    message: "Korisničko ime je obavezno!",
+  }),
+  email: z.string().email({
+    message: "Email je obavezan!",
+  }),
+  password: z.string().min(6, {
+    message: "Minimum 6 slova!",
+  }),
+});
