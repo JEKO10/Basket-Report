3d9fda0fb8f352df4b96b95cea21f615d244373d
Aleksa <aleksa3b@gmail.com>
2024-09-14 17:43:50 +0200

    about us page

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -2,9 +2,34 @@ import React from "react";
 
 const AboutPage = () => {
   return (
-    <div>
-      <p>AboutPage</p>
-    </div>
+    <article className="[&>p]:text-xl [&>p]:text-md [&>p]:italic [&>p]:font-normal [&>p]:text-[#6eabda] [&>p]:my-5">
+      <h2 className="font-lusitana text-4xl mt-5 mb-1">O nama</h2>
+      <p>
+        Dobrodošli na našu platformu, mjesto gdje se turniri rađaju i takmičari
+        okupljaju! Naša misija je da vam omogućimo jednostavan način za
+        kreiranje kostura za turnire, bilo da organizujete prijateljski sportski
+        turnir, gaming ligu, ili bilo koje drugo takmičenje.
+      </p>
+      <p>
+        Vjerujemo da svaki turnir zaslužuje jasnu strukturu i profesionalan
+        pristup, bez komplikacija. Naša platforma vam omogućava da kreirate
+        prilagođene turnirske kosture u samo nekoliko koraka, upravljate
+        učesnicima, pratite rezultate, i osigurate da svaka faza takmičenja teče
+        glatko.
+      </p>
+      <p>
+        Naš tim je posvećen stalnom unapređenju i inovaciji, kako bismo vam
+        omogućili najbolje korisničko iskustvo. Sa ljubavlju prema takmičenju i
+        tehnologiji, stvorili smo alat koji je jednostavan za korišćenje, a
+        dovoljno fleksibilan za sve vrste turnira.
+      </p>
+      <p>
+        Bilo da ste iskusni organizator ili tek započinjete svoj prvi turnir,
+        naša platforma je tu da vam pomogne da svaki korak bude lakši i
+        efikasniji. Pridružite se našoj zajednici i kreirajte turnir koji će svi
+        pamtiti!
+      </p>
+    </article>
   );
 };
 
