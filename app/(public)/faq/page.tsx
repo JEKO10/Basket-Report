09d5c6d003eb38d29e4fdb4d8ee882f2da19113c
Aleksa <aleksa3b@gmail.com>
2024-09-15 20:35:12 +0200

    faq page

--- app/(public)/faq/page.tsx
+++ app/(public)/faq/page.tsx
@@ -1,7 +1,64 @@
 import React from "react";
 
 const FaqPage = () => {
-  return <div>FaqPage</div>;
+  return (
+    <section>
+      <h2 className="font-lusitana text-4xl mt-5 mb-1">
+        Često postavljana pitanja
+      </h2>
+      <article>
+        <div>
+          <h3>1. Kako kreirati turnir na vašoj platformi?</h3>
+          <p>
+            Da biste kreirali turnir, potrebno je da se prijavite na svoj nalog,
+            odaberete opciju Kreiraj turnir i pratite korake za podešavanje
+            formata, broja učesnika i pravila. Sve je brzo i jednostavno!
+          </p>
+        </div>
+        <div>
+          <h3>2. Koji formati turnira su podržani?</h3>
+          <p>
+            Naša platforma podržava različite formate turnira, uključujući
+            eliminacioni format, round-robin, kao i kombinovane sisteme. Možete
+            prilagoditi format prema svojim potrebama.
+          </p>
+        </div>
+        <div>
+          <h3>3. Kako mogu pratiti rezultate tokom turnira?</h3>
+          <p>
+            Rezultati su automatski ažurirani nakon svakog meča. Kao
+            organizator, možete ručno uneti rezultate, dok će učesnici i
+            posetioci imati pristup rezultatima u realnom vremenu.
+          </p>
+        </div>
+        <div>
+          <h3>4. Da li je platforma besplatna za korišćenje?</h3>
+          <p>
+            Osnovne funkcionalnosti su potpuno besplatne. Takođe nudimo napredne
+            opcije za prilagođavanje turnira i dodatne funkcije kroz premium
+            planove.
+          </p>
+        </div>
+        <div>
+          <h3>
+            5. Koji su minimalni i maksimalni brojevi učesnika po turniru?
+          </h3>
+          <p>
+            Minimalni broj učesnika je 2, dok je maksimalni broj ograničen na
+            256 učesnika.
+          </p>
+        </div>
+        <div>
+          <h3>6. Kako mogu kontaktirati podršku?</h3>
+          <p>
+            Ako imate bilo kakvih pitanja ili problema, možete nas kontaktirati
+            putem emaila ili putem kontakt forme na sajtu. Naš tim za podršku je
+            tu da vam pomogne.
+          </p>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default FaqPage;
