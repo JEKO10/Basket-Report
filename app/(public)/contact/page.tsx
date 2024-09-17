e24ac0bb36ccbd26e33a50272743099072bbeed9
Aleksa <aleksa3b@gmail.com>
2024-09-17 21:24:45 +0200

    contact page

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -1,7 +1,57 @@
 import React from "react";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <section>
+      <header className="flex items-end justify-start font-lusitana bg-primary px-6 py-4 h-36 rounded-lg">
+        <p className="text-4xl text-white">Kontakt</p>
+      </header>
+      <form className="flex items-center justify-center flex-col bg-accent mt-2 px-8 pt-7 pb-5 rounded-lg">
+        <div className="relative w-full mb-3">
+          <input
+            type="text"
+            id="email"
+            placeholder=" "
+            className="peer w-full focus:pt-5 px-3 py-2 rounded-md outline-none transition-all duration-100 ease"
+          />
+          <label
+            htmlFor="email"
+            className="absolute left-3 text-md text-gray-500 transition-all duration-100 ease peer-placeholder-shown:top-1 peer-placeholder-shown:text-md peer-focus:top-0 peer-focus:text-xs peer-focus:text-blue-500 select-none py-1"
+          >
+            Vaše ime
+          </label>
+        </div>
+        <div className="relative w-full mb-3">
+          <input
+            type="text"
+            id="email"
+            placeholder=" "
+            className="peer w-full focus:pt-5 px-3 py-2 rounded-md outline-none transition-all duration-100 ease"
+          />
+          <label
+            htmlFor="email"
+            className="absolute left-3 top-0 text-md text-gray-500 transition-all duration-100 ease peer-placeholder-shown:top-1 peer-placeholder-shown:text-md peer-focus:top-0 peer-focus:text-xs peer-focus:text-blue-500 select-none py-1"
+          >
+            Vaša e-mail adresa
+          </label>
+        </div>
+        <label className="flex items-start justify-center flex-col w-full mb-3">
+          <span className="mb-1.5 text-md select-none">Poruka</span>
+          <textarea
+            placeholder="Poruka..."
+            className="w-full px-4 py-3 rounded-md outline-none resize-none"
+            rows={7}
+          />
+        </label>
+        <button
+          type="submit"
+          className="flex justify-center items-center bg-primary mt-4 w-full text-text text-lg py-2 px-3 rounded-md transition hover:bg-primary/65"
+        >
+          Pošalji
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default ContactPage;
