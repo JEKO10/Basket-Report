b5b546b0c20657dbae6d5eab63688e8195e0a6a1
Aleksa <aleksa3b@gmail.com>
2024-09-18 15:31:19 +0200

    refactor labels to use pseudo-classes instead of peer utility

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -7,49 +7,35 @@ const ContactPage = () => {
 
   return (
     <section>
-      <header className="flex items-end justify-start font-lusitana bg-primary px-6 py-4 h-36 rounded-lg">
+      <header className="header-section">
         <p className="text-4xl text-white">Kontakt</p>
       </header>
-      <form className="flex items-center justify-center flex-col bg-accent mt-2 px-8 pt-7 pb-5 rounded-lg">
-        <div className="relative w-full mb-3 bg-primary rounded-md">
+      <form className="form-section">
+        <div className="input-container">
           <input
             type="text"
             id="name"
             placeholder=" "
-            className={`peer w-full focus:pt-5 px-3 py-2 rounded-md outline-none transition-all duration-100 ease ${hasValue.name && "pt-5"}`}
+            className={`input-base ${hasValue.name ? "pt-5" : ""}`}
             onChange={(e) =>
               setHasValue({ ...hasValue, name: e.target.value !== "" })
             }
           />
-          <label
-            htmlFor="name"
-            className={`absolute left-3 top-0 text-md text-gray-500 transition-all duration-100 ease select-none py-1 cursor-text ${
-              hasValue.name
-                ? "text-xs peer-focus:text-blue-500"
-                : "peer-placeholder-shown:top-1 peer-placeholder-shown:text-md peer-focus:top-0 peer-focus:text-xs peer-focus:text-blue-500"
-            }`}
-          >
+          <label htmlFor="name" className="label-base">
             Vaše ime
           </label>
         </div>
-        <div className="relative w-full mb-3 bg-primary rounded-md">
+        <div className="input-container">
           <input
             type="email"
             id="email"
             placeholder=" "
-            className={`peer w-full focus:pt-5 px-3 py-2 rounded-md outline-none transition-all duration-100 ease ${hasValue.email && "pt-5"}`}
+            className={`input-base ${hasValue.email ? "pt-5" : ""}`}
             onChange={(e) =>
               setHasValue({ ...hasValue, email: e.target.value !== "" })
             }
           />
-          <label
-            htmlFor="email"
-            className={`absolute left-3 top-0 text-md text-gray-500 transition-all duration-100 ease select-none py-1 cursor-text ${
-              hasValue.email
-                ? "text-xs peer-focus:text-blue-500"
-                : "peer-placeholder-shown:top-1 peer-placeholder-shown:text-md peer-focus:top-0 peer-focus:text-xs peer-focus:text-blue-500"
-            }`}
-          >
+          <label htmlFor="email" className="label-base">
             Vaša e-mail adresa
           </label>
         </div>
@@ -63,7 +49,8 @@ const ContactPage = () => {
         </label>
         <button
           type="submit"
-          className="flex justify-center items-center bg-primary mt-4 w-full text-text text-lg py-2 px-3 rounded-md transition hover:bg-primary/65"
+          className="button-base"
+          onClick={(e) => e.preventDefault()}
         >
           Pošalji
         </button>
