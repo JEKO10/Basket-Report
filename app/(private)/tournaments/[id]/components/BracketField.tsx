5c6251f4152c41c79ede3c5ffb0f0a11876fd55c
Aleksa <aleksa3b@gmail.com>
2024-09-24 20:32:54 +0200

    handleBracket function for making brackets

--- app/(private)/tournaments/[id]/components/BracketField.tsx
+++ app/(private)/tournaments/[id]/components/BracketField.tsx
@@ -2,23 +2,23 @@ import React from "react";
 
 const BracketField = ({
   index,
-  isEvenPair,
+  match,
   teamName,
 }: {
-  index: number;
-  isEvenPair?: boolean;
+  index?: number;
+  match: number;
   teamName?: string;
 }) => {
   return (
     <article
-      className={`bg-[#6EABDA] h-7 w-36 p-1 relative ${isEvenPair ? "mb-5" : "mb-0.5"}`}
+      className={`bg-[#6EABDA] h-7 w-36 p-1 relative ${index ? "mb-5" : "mb-0.5"}`}
     >
       <div className="flex justify-start items-center bg-column h-full w-full overflow-hidden">
         {teamName && teamName?.length > 15 && (
           <div className="absolute inset-0 bg-gradient-to-r from-transparent via-transparent to-[#6EABDA] to-[120%]" />
         )}
         <div className="bg-primary h-full min-w-5 flex justify-center items-center">
-          <p className="text-sm text-white select-none">{index}</p>
+          <p className="text-sm text-white select-none">{match}</p>
         </div>
         <p className="text-white pl-1 pb-0.5 tracking-wider [text-shadow:_2px_2px_1px_#222]">
           {teamName}
--- app/(private)/tournaments/[id]/page.tsx
+++ app/(private)/tournaments/[id]/page.tsx
@@ -6,20 +6,44 @@ import { getUserByid } from "@/actions/user";
 
 import BracketField from "./components/BracketField";
 
+const nextRound = (seed: number, participantsCount: number) => {
+  return seed <= participantsCount ? seed : 0;
+};
+
+const handleBracket = (participants: number) => {
+  const rounds = Math.ceil(Math.log(participants) / Math.log(2));
+
+  if (participants < 2) {
+    return [];
+  }
+
+  let matches = [[1, 2]];
+
+  for (let round = 1; round < rounds; round++) {
+    let roundMatches = [];
+    let sum = Math.pow(2, round + 1) + 1;
+
+    for (let i = 0; i < matches.length; i++) {
+      let home = nextRound(matches[i][0], participants);
+      let away = nextRound(sum - matches[i][0], participants);
+      roundMatches.push([home, away]);
+
+      home = nextRound(sum - matches[i][1], participants);
+      away = nextRound(matches[i][1], participants);
+      roundMatches.push([home, away]);
+    }
+
+    matches = roundMatches;
+  }
+
+  return matches;
+};
+
 const SingleTournamentPage = async ({ params }: { params: { id: string } }) => {
   const data = await getTournamentsById(params.id);
   const user = await getUserByid(data?.creatorId);
 
-  const tournamentIndices = [];
-  const participants = data?.participants || 0;
-
-  for (let i = 0; i < Math.ceil(participants / 2); i++) {
-    tournamentIndices.push(i + 1);
-
-    if (participants - i > i + 1) {
-      tournamentIndices.push(participants - i);
-    }
-  }
+  const bracket = handleBracket(data?.participants || 0);
 
   return (
     <section className="p-8">
@@ -41,17 +65,14 @@ const SingleTournamentPage = async ({ params }: { params: { id: string } }) => {
       </header>
       <article>
         {!data?.bracketSize ? (
-          data?.teams.map((team, index) => (
-            <BracketField key={team} index={index} teamName={team} />
-          ))
+          ""
         ) : (
           <>
-            {tournamentIndices.map((index, idx) => (
-              <BracketField
-                key={index}
-                index={index}
-                isEvenPair={idx % 2 === 1}
-              />
+            {bracket.map((match, index) => (
+              <section key={index}>
+                <BracketField match={match[0]} />
+                <BracketField index={index + 1} match={match[1]} />
+              </section>
             ))}
           </>
         )}
