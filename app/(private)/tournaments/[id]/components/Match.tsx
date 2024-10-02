872c7fd12171d8e45859804219e74bce8f8599d7
Aleksa <aleksa3b@gmail.com>
2024-10-02 17:28:01 +0200

    add score button only if both teams exist

--- app/(private)/tournaments/[id]/components/Match.tsx
+++ app/(private)/tournaments/[id]/components/Match.tsx
@@ -31,38 +31,34 @@ const Match = ({
     teamB: 0,
   });
 
-  const teamAExists = teams && teams[match[0] - 1];
-  const teamBExists = teams && teams[match[1] - 1];
+  const bothTeamsExist = match[0] !== null && match[1] !== null;
 
   return (
     <>
       <section className="mb-10 relative">
         <BracketField match={match[0]} team={teams && teams[match[0] - 1]} />
         <BracketField match={match[1]} team={teams && teams[match[1] - 1]} />
-        {teamAExists &&
-          teamBExists &&
-          score.teamA === 0 &&
-          score.teamB === 0 && (
-            <div
-              onClick={() => setIsModalOpen(true)}
-              className="flex justify-center items-center bg-[#6EABDA] text-white h-full absolute top-0 right-0 px-1 transition-colors cursor-pointer hover:text-black"
-            >
-              <TbEdit className="text-2xl" title="Dodaj rezultat" />
-            </div>
-          )}
+        {bothTeamsExist && score.teamA === 0 && score.teamB === 0 && (
+          <div
+            onClick={() => setIsModalOpen(true)}
+            className="flex justify-center items-center bg-[#6EABDA] text-white h-full absolute top-0 right-0 px-1 transition-colors cursor-pointer hover:text-black"
+          >
+            <TbEdit className="text-2xl" title="Dodaj rezultat" />
+          </div>
+        )}
         {score.teamA !== 0 && score.teamB !== 0 && (
           <div
             onClick={() => setIsModalOpen(true)}
             className="flex justify-center items-start flex-col bg-[#6EABDA] text-white h-full w-7 absolute top-0 right-0 overflow-hidden"
           >
             <p
-              className={`${score.teamA && score.teamA.toString().length > 2 ? "text-start pl-1" : "text-center pl-0"} w-full`}
+              className={`${score.teamA && score.teamA.toString().length > 2 ? "text-start pl-1" : "text-center pl-0"} w-full select-none`}
             >
               {score.teamA}
             </p>
             <div className="h-0.5 w-full my-0.5 bg-body z-10" />
             <p
-              className={`${score.teamB && score.teamB.toString().length > 2 ? "text-start pl-1" : "text-center pl-0"} w-full`}
+              className={`${score.teamB && score.teamB.toString().length > 2 ? "text-start pl-1" : "text-center pl-0"} w-full select-none`}
             >
               {score.teamB}
             </p>
