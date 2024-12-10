643b1f21ff54c39ea1bb21cdca5bd1f4f39a2cc1
Aleksa <aleksa3b@gmail.com>
2024-12-10 23:56:13 +0100

    ordinal numbers for adding team names

--- components/createTournament/BracketSizeFormat.tsx
+++ components/createTournament/BracketSizeFormat.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useRef, useState } from "react";
 import { PiMinus, PiPlus } from "react-icons/pi";
 import * as z from "zod";
 
@@ -21,6 +23,38 @@ const BracketSizeFormat = ({
     React.SetStateAction<z.infer<typeof TournamentSchema>>
   >;
 }) => {
+  const [content, setContent] = useState<string>("");
+  const scrollRef = useRef<HTMLTextAreaElement>(null);
+
+  const getLineNumbers = () => {
+    const lines = content.split("\n");
+    let lineCount = 0;
+
+    return lines
+      .map((line) => {
+        if (line.trim() !== "") {
+          lineCount += 1;
+          return `${lineCount}.`;
+        }
+        return "";
+      })
+      .join("\n");
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.target.value);
+  };
+
+  const syncScroll = () => {
+    if (scrollRef.current) {
+      const scrollTop = scrollRef.current.scrollTop;
+      const lineNumberDiv = document.getElementById("line-numbers");
+      if (lineNumberDiv) {
+        lineNumberDiv.scrollTop = scrollTop;
+      }
+    }
+  };
+
   if (isBlank) {
     return (
       <section>
@@ -65,9 +99,21 @@ const BracketSizeFormat = ({
         Jedan ispod drugog, od najboljeg ka najlošijem
       </p>
       <textarea
-        className="bg-primary text-base text-text w-full md:w-1/2 h-60 my-2 py-4 px-5 resize-none rounded-md outline-none placeholder-text"
+        value={getLineNumbers()}
+        readOnly
+        id="line-numbers"
+        className="bg-primary resize-none overflow-hidden h-60 w-8 my-2 py-2 pl-2 text-text text-base rounded-l-md"
+      />
+      <textarea
+        value={content}
+        onChange={(event) => {
+          handleChange(event);
+          setTeams(event.target.value);
+        }}
+        ref={scrollRef}
+        onScroll={syncScroll}
+        className="bg-primary text-base text-text w-full md:w-1/2 h-60 my-2 py-2 px-3 resize-none rounded-r-md outline-none placeholder-text"
         placeholder="Unesite učesnike, jedan učesnik ispod drugog"
-        onChange={(event) => setTeams(event.target.value)}
       />
       <label className="flex align-center justify-start flex-col w-fit">
         <div className="flex align-center justify-start cursor-pointer">
